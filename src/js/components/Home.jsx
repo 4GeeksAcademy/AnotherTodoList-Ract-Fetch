5594ca533284a147b6a3749529e34cdfa19a3450
fl-armable <fl.irlanda@gmail.com>
2025-05-16 06:31:15 +0000

    Second commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import {useState} from "react";
 import ToDoList from "./ToDoList";
 
 const Home = () => {
--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 const ToDoList = ({shareUser}) => {
 	const [taskList, setTaskList] = useState([]);
